9cc0c272c134b74aaedf625e378c6e032647e5b3
Kristian Hartikainen <kristian@statwing.com>
2015-03-21 00:50:32 +0200

    Correctly load autosave.el. Removed .emacs.d from load path to get rid of the error

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
-(add-to-list 'load-path "~/.emacs.d/")
-(add-to-list 'load-path "~/.emacs.d/lisp")
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 
+(load-file "~/.emacs.d/lisp/autosave.el")
 (load-file "~/.emacs.d/packages.el")
 (load-file "~/.emacs.d/platform.el")
