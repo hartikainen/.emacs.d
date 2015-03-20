0065b63e1b3b2c1c79b9b0fb3f990a6e5ec025eb
Kristian Hartikainen <kristian@statwing.com>
2015-03-21 00:17:03 +0200

    Platform specific stuff. Initially only for mac

---
+++ platform/mac.el
@@ -0,0 +1,19 @@
+(message "loading mac specific settings")
+
+(setq shell-file-name "bash")
+(setenv "SHELL" shell-file-name)
+
+;;
+;; someone's blog on copy and pasting between mac and emacs
+;;
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
