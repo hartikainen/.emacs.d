a73adadd552e7a474afa0fbd7b203fcc05dd8199
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2015-03-23 00:48:01 +0200

    Custom set magit diff's add and del colors. Not sure if these should be kept in the init.el or not.

--- init.el
+++ init.el
@@ -10,3 +10,18 @@
 
 (setq mail-archive-file-name "~/.emacs.d/sentmail")
 (defconst user-mail-address "kristian.hartikainen@gmail.com")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-diff-add ((t (:inherit diff-added :foreground "cyan"))))
+ '(magit-diff-del ((t (:inherit diff-removed :foreground "red"))))
+ '(magit-item-highlight ((t (:inherit secondary-selection :foreground "black")))))
