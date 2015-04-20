607094dd883037a2f97fff5a769cddb3b3272769
Kristian Hartikainen <kristian@statwing.com>
2015-04-20 13:00:41 +0300

    Added list of packages

--- packages.el
+++ packages.el
@@ -8,3 +8,19 @@
 	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 (package-initialize)
+
+(defvar my-packages
+      '(
+        auctex
+        magit
+	multiple-cursors
+	))
+
+(mapc
+ (lambda (package)
+   (or (package-installed-p package)
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+           (package-install package))))
+ my-packages)
+
+(add-to-list 'load-path "~/.emacs.d/lisp")
