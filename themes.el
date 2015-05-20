ae225b6d59149d4a34547b93c43aa73c3856f795
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2015-05-20 22:29:36 +0300

    Customize the line highlight to work with monokai

--- themes.el
+++ themes.el
@@ -10,3 +10,10 @@
 ;;        (color-theme-initialize)
 ;;        (color-theme-hober)))
 ;;   )
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(hl-line ((t (:inherit t :background "#303030")))))
