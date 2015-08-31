880f1e50d8b1928bebd1dc6b1ace1217127662f8
Kristian Hartikainen <kristian.hartikainen@aalto.fi>
2015-08-31 15:54:46 +0300

    Linux: Add functions to copy stuff to/from clipboard. Doesn't work on other OS's

--- functions.el
+++ functions.el
@@ -35,3 +35,32 @@ Interactively, N is the prefix arg."
     (back-to-indentation)
     (if (equal (point) previous-point) (move-beginning-of-line 1)))
   )
+
+;; Clipboard functions
+(defun copy-to-clipboard ()
+  (interactive)
+  (if (display-graphic-p)
+      (progn
+        (message "Yanked region to x-clipboard!")
+        (call-interactively 'clipboard-kill-ring-save)
+        )
+    (if (region-active-p)
+        (progn
+          (shell-command-on-region (region-beginning) (region-end) "xsel -i -b")
+          ;;(shell-command-on-region (region-beginning) (region-end) "xclip -i -selection clipboard")
+          (message "Yanked region to clipboard!")
+          (deactivate-mark))
+      (message "No region active; can't yank to clipboard!")))
+  )
+
+(defun paste-from-clipboard ()
+  (interactive)
+  (if (display-graphic-p)
+      (progn
+        (clipboard-yank)
+        (message "graphics active")
+        )
+    (insert (shell-command-to-string "xsel -o -b"))
+    ;;(insert (shell-command-to-string "xclip -o -selection clipboard"))
+    )
+  )
