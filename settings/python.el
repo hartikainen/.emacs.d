5e60bf968e213875b82aa4ce1c173d8fecd44b98
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2017-09-17 17:56:51 -0700

    Nose keybinding change

--- settings/python.el
+++ settings/python.el
@@ -3,9 +3,10 @@
             (setq indent-tabs-mode nil
                   tab-width 2
                   python-indent 2)
-            (local-set-key (kbd "C-c a") 'nosetests-all)
-            (local-set-key (kbd "C-c m") 'nosetests-module)
-            (local-set-key (kbd "C-c s") 'nosetests-one)))
+            (local-set-key (kbd "C-c c a") 'nosetests-all)
+            (local-set-key (kbd "C-c c m") 'nosetests-module)
+            (local-set-key (kbd "C-c c s") 'nosetests-one)
+            (local-set-key (kbd "C-c C-c") 'compile)))
 
 (setq auto-mode-alist (cons '("\\.pyx$" . python-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.pxd$" . python-mode) auto-mode-alist))
