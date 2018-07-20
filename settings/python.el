b4da726c9090778a6953c07eb85eff3fe918f690
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-07-20 10:51:50 -0700

    Add python indent offset guessing and automatic tab-mode inference

--- settings/python.el
+++ settings/python.el
@@ -1,3 +1,27 @@
+(defun how-many-region (begin end regexp &optional interactive)
+  "Print number of non-trivial matches for REGEXP in region.
+   Non-interactive arguments are Begin End Regexp"
+  (interactive "r\nsHow many matches for (regexp): \np")
+  (let ((count 0) opoint)
+    (save-excursion
+      (setq end (or end (point-max)))
+      (goto-char (or begin (point)))
+      (while (and (< (setq opoint (point)) end)
+                  (re-search-forward regexp end t))
+        (if (= opoint (point))
+            (forward-char 1)
+          (setq count (1+ count))))
+      (if interactive (message "%d occurrences" count))
+      count)))
+
+(defun infer-indentation-style ()
+  ;; if our source file uses tabs, we use tabs, if spaces spaces, and if
+  ;; neither, we use the current indent-tabs-mode
+  (let ((space-count (how-many-region (point-min) (point-max) "^  "))
+        (tab-count (how-many-region (point-min) (point-max) "^\t")))
+    (if (> space-count tab-count) (setq indent-tabs-mode nil))
+    (if (> tab-count space-count) (setq indent-tabs-mode t))))
+
 (defun nosetests-all-virtualenv ()
   (interactive)
   (let ((nose-global-name
@@ -16,7 +40,9 @@
           (lambda ()
             (setq indent-tabs-mode nil
                   tab-width 4
-                  python-indent-offset 4)
+                  python-indent-offset 4
+                  python-indent-guess-indent-offset t)
+            (infer-indentation-style)
             (local-set-key (kbd "C-c c a") 'nosetests-all)
             (local-set-key (kbd "C-c c m") 'nosetests-module)
             (local-set-key (kbd "C-c c s") 'nosetests-one)
