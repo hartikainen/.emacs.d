61ad5130c700cd3595922fff141a0171f0b16462
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-09-19 14:42:41 -0700

    Add todos to org mode

--- settings/org-mode.el
+++ settings/org-mode.el
@@ -5,6 +5,7 @@
 (global-set-key (kbd "C-c o a") 'org-agenda)
 (global-set-key (kbd "C-c o c") 'org-capture)
 (global-set-key (kbd "C-c o l") 'org-store-link)
+(global-set-key (kbd "C-c o t") 'org-todo-list)
 
 (defun capture-report-date-file (path)
   (let ((name (read-string "Filename: ")))
@@ -12,9 +13,20 @@
                               (format-time-string "%Y-%m-%d")
                               name) path)))
 
+(setq org-blank-before-new-entry
+      '((heading . always)
+       (plain-list-item . always)))
+
 (setq org-capture-templates
-      '(("n" "Note" entry
-         (file (capture-report-date-file  org-directory))
-         "* %?Notes from %U\n\n  %i\n %a")))
+ '(("n" "Note" entry
+    (file (capture-report-date-file  org-directory))
+    "* %?Notes from %U\n\n  %i\n %a")
+   ("t" "Todo" entry (file+headline "~/my-notes/todos.org" "Tasks")
+    "* TODO %?\n\n  %i" :prepend t)
+   ("e" "Experiment" entry (file+headline "~/my-notes/experiments.org" "Experiments")
+    "* EXPERIMENT %?\n  %i\n\n\n" :prepend t)
+   ("j" "Journal" entry (file+olp+datetree "~/my-notes/journals.org")
+    "* %?\nEntered on %U\n  %i\n  %a" :prepend t)))
 
 (setq org-log-done t)
+(setq org-startup-folded nil)
