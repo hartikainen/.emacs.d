f933c20989ccf938a8dc19df33b80f2dd85c04ca
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-05-13 21:11:17 +0100

    Add copy-strip-whitespace function

--- functions.el
+++ functions.el
@@ -83,3 +83,21 @@ Interactively, N is the prefix arg."
      (region-end))
     (downcase-region (region-beginning)
                      (region-end))))
+
+(defun copy-strip-whitespace (&optional beg end)
+  "Save the current region (or line) to the `kill-ring' after stripping extra whitespace and new lines"
+  (interactive
+   (if (region-active-p)
+       (list (region-beginning) (region-end))
+     (list (line-beginning-position) (line-end-position))))
+  (let ((my-text (buffer-substring-no-properties beg end)))
+    (with-temp-buffer
+      (insert my-text)
+      (goto-char 1)
+      (while (looking-at "[ \t\n]")
+        (delete-char 1))
+      (let ((fill-column 9333999))
+        (fill-region (point-min) (point-max)))
+      (set-mark (point-min))
+      (goto-char (point-max))
+      (clipetty-kill-ring-save))))
